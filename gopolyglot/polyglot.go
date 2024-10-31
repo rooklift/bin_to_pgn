e2192b69762a102c761b93b149227139d753527f
rooklift <16438795+rooklift@users.noreply.github.com>
2024-10-31 10:07:40 +0000

    ParseMove(), also check for ordered keys

--- gopolyglot/polyglot.go
+++ gopolyglot/polyglot.go
@@ -1,8 +1,12 @@
 package gopolyglot
 
+// TODO - remember polyglot uses e1h1 castling format.
+// Remember to check such moves actually have a king there (rather than being some other piece).
+
 import (
 	"bufio"
 	"encoding/binary"
+	"fmt"
 	"os"
 )
 
@@ -13,7 +17,14 @@ type Entry struct {
 	Learn	uint32
 }
 
+func (self *Entry) MoveString() string {
+	return ParseMove(self.Move)
+}
+
 func LoadFromFile(filepath string) ([]Entry, error) {
+
+	var previous_key uint64 = 0
+	var have_warned bool = false
 
 	var ret []Entry
 
@@ -31,41 +42,52 @@ func LoadFromFile(filepath string) ([]Entry, error) {
 		if err != nil {
 			break
 		}
+		if !have_warned && entry.Key < previous_key {
+			fmt.Fprintf(os.Stderr, "Warning: book was not in order! This will invalidate results.\n")
+			have_warned = true			// TODO - maybe we should just sort the thing.
+		}
+		previous_key = entry.Key
 		ret = append(ret, entry)
 	}
 
 	return ret, nil
 }
 
-/*
+func ParseMove(b uint16) string {
 
-func ReadBigEndian64(b []byte) uint64 {
-	var ret uint64
-	ret += uint64(b[0]) << 56
-	ret += uint64(b[1]) << 48
-	ret += uint64(b[2]) << 40
-	ret += uint64(b[3]) << 32
-	ret += uint64(b[4]) << 24
-	ret += uint64(b[5]) << 16
-	ret += uint64(b[6]) <<  8
-	ret += uint64(b[7]) <<  0
-	return ret
+	to_file   := b >>  0 & 0b111
+	to_row    := b >>  3 & 0b111
+	from_file := b >>  6 & 0b111
+	from_row  := b >>  9 & 0b111
+	promotion := b >> 12 & 0b111
+
+	to_file_c := 'a' + to_file
+	to_row_c  := '1' + to_row
+
+	from_file_c := 'a' + from_file
+	from_row_c  := '1' + from_row
+
+	prom_string := ""
+
+	switch promotion {
+		case 1: prom_string = "n"
+		case 2: prom_string = "n"
+		case 3: prom_string = "n"
+		case 4: prom_string = "n"
+		default: prom_string = ""
+	}
+
+	return fmt.Sprintf("%c%c%c%c%s", from_file_c, from_row_c, to_file_c, to_row_c, prom_string)
 }
 
-func ReadBigEndian32(b []byte) uint32 {
-	var ret uint32
-	ret += uint32(b[0]) << 24
-	ret += uint32(b[1]) << 16
-	ret += uint32(b[2]) <<  8
-	ret += uint32(b[3]) <<  0
+func Probe(book []Entry, key uint64) []Entry {
+
+	var ret []Entry
+
+	// lower := 0
+	// upper := len(book) - 1
+
+	// TODO - binary search
+
 	return ret
 }
-
-func ReadBigEndian16(b []byte) uint16 {
-	var ret uint16
-	ret += uint16(b[0]) << 8
-	ret += uint16(b[1]) << 0
-	return ret
-}
-
-*/
