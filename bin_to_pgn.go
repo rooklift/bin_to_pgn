81bd7f024f7cb65872d833e69e36be8f5bf26ab7
rooklift <16438795+rooklift@users.noreply.github.com>
2024-11-01 23:07:01 +0000

    first semi-working version

--- bin_to_pgn.go
+++ bin_to_pgn.go
@@ -18,22 +18,14 @@ func main() {
 		return
 	}
 
-	fmt.Printf("Book length: %d\n", len(book.Entries))
+	// fmt.Printf("Book length: %d\n", len(book.Entries))
 
 	node := tree.NewNode(nil, "")
 	board, _ := gcb.BoardFromFEN("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1")
 
 	recurse(node, board, book, 0)
-	fmt.Printf("%v nodes\n", node.CountNodes())
-
-	for {
-		fmt.Printf("%v ", node.Move)
-		if len(node.Children) == 0 {
-			fmt.Printf("\n")
-			break
-		}
-		node = node.Children[0]
-	}
+	// fmt.Printf("%v nodes\n", node.CountNodes())
+	fmt.Printf("%v\n", make_string(node, false))
 }
 
 func recurse(node *tree.Node, board *gcb.Board, book *poly.Book, depth int) {
@@ -52,3 +44,33 @@ func recurse(node *tree.Node, board *gcb.Board, book *poly.Book, depth int) {
 		recurse(new_node, new_board, book, depth + 1)
 	}
 }
+
+func make_string(node *tree.Node, skip_self bool) string {
+
+	if len(node.Children) == 0 {
+		if skip_self {
+			return " "
+		} else {
+			return node.Move + " "
+		}
+	} else if len(node.Children) == 1 {
+		if skip_self {
+			return make_string(node.Children[0], false) + " "
+		} else {
+			return node.Move + " " + make_string(node.Children[0], false) + " "
+		}
+	} else {
+		var s string
+		if skip_self {
+			s = node.Children[0].Move + " "
+		} else {
+			s = node.Move + " " + node.Children[0].Move + " "
+		}
+		for _, child := range node.Children[1:] {
+			s += "( " + make_string(child, false) + ") "
+		}
+		s += make_string(node.Children[0], true)
+		return s
+	}
+}
+
